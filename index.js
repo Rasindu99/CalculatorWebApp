c44ca7015e5a2ae30d377762c247df218650a16a
Rasindu Sandeep <rasindu0823@gmail.com>
2024-06-08 21:05:24 +0530

    Basic_Cal_Version_1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Hello Calculator");
+
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+
+const appendToDisplay = (input) => {
+ display.value = display.value + input;
+}
+
+console.log(display.value);
+
+const clearDisplay = () => {
+  display.value = "";
+}
+
+const calculate = () => {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error"
+  }
+}
